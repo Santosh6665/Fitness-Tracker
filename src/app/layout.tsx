4e270202b20d2bc856f0676a1dfc11293e6531d0
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-09 10:22:43 +0000

    initial-scale=0.7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.7" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
