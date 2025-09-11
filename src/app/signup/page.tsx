66b1ee04a9163b64188585acc5852386b131cd09
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-11 06:25:05 +0000

    Try fixing this error: `Build Error: Error: x Unexpected token `div`. Ex

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -139,7 +139,7 @@ export default function SignUpPage() {
                   login
                 </Link>{' '}
                 instead.
-              </Alerc/components/icons/logo.tsxertDescription>
+              </AlertDescription>
             </Alert>
           )}
           <Form {...form}>
