e9a62cea38a5a0e018738bd8289bcccc24802fd8
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-11 13:14:47 +0000

    stop zooming feature in all page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
   icons: {
     icon: '/logo.png',
   },
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 };
 
 export default function RootLayout({
