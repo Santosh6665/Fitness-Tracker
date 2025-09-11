87c475314b52eb2c53da1d91afb27456e7dfd7aa
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-11 13:06:43 +0000

    not fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: 'AI Powered Fitness Tracker',
   description: 'Your personalized guide to fitness.',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
   icons: {
     icon: '/logo.png',
   },
@@ -23,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
